5831b8b956e7e4ddeb55e0995f87cfe65c5748de
roman-valov-1 <62258593+roman-valov-1@users.noreply.github.com>
2022-04-02 17:40:27 +0300

    ...

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,14 +3,19 @@ import React, { useState } from 'react';
 function Categories({ items, getActiveItem }) {
    const [activeItem, setActiveItem] = useState(null);
 
+   const onSelectItem = (index) => {
+      setActiveItem(index)
+   };
+
+
    return (
       <div className="categories">
          <ul>
             <li className={activeItem === null ? 'active' : ''}
-               onClick={() => setActiveItem(null)}>Все</li>
+               onClick={() => onSelectItem(null)}>Все</li>
             {items && items.map( (item, index) => (
                <li className={activeItem === index ? 'active' : ''}
-               onClick={() => setActiveItem(index)} 
+               onClick={() => onSelectItem(index)} 
                key={`${item}_${index}`} >{item}</li> 
             ))}
          </ul>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import logoSvg from '../assets/img/pizza-logo.svg';
 import Button from './Button';
+import {Link} from 'react-router-dom';
 
 function Header() {
    return (
       <div className="header">
          <div className="container">
-            <div className="header__logo">
-               <img width="38" src={logoSvg} alt="Pizza logo" />
-               <div>
-                  <h1>React Pizza</h1>
-                  <p>самая вкусная пицца во вселенной</p>
+            <Link to="/">
+               <div className="header__logo">
+                  <img width="38" src={logoSvg} alt="Pizza logo" />
+                  <div>
+                     <h1>React Pizza</h1>
+                     <p>самая вкусная пицца во вселенной</p>
+                  </div>
                </div>
-            </div>
+            </Link>
             <div className="header__cart">
-               <Button className="button--cart">
+               <Link to='/cart'>
+                  <Button className="button--cart">
                   <span>520 ₽</span>
                   <div className="button__delimiter"></div>
                   <svg
@@ -47,7 +51,8 @@ function Header() {
                      />
                   </svg>
                   <span>3</span>
-               </Button>
+                  </Button>
+               </Link>
             </div>
          </div>
       </div>
