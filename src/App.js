02ca8052036a35795ea6af364d6953e9a82676be
roman-valov-1 <62258593+roman-valov-1@users.noreply.github.com>
2022-04-02 21:04:27 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import  axios  from "axios";
 
 import { Header } from './components/imports';
-import Home from './pages/Home';
-import Cart from './pages/Cart';
+import { Home, Cart } from './pages/imports';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({data}) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
+
+  console.log(pizzas);
+  
   return (
     <BrowserRouter>
       <div className="wrapper">
         <Header />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home items={pizzas}/>} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
         </div>
